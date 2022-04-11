ed8b72feb4adeaf6992f97b474b27b093a7f7032
Ange-A <angeaduhire@gmail.com>
2022-04-10 17:27:46 -0700

    sleep

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,3 +11,5 @@ printf("Size of an int :%d byte(S)\n", sizeof(int));
 printf("Size of a long int :%d byte(S)\n", sizeof(long int));
 printf("Size of a long long int :%d byte(S)\n", sizeof(long long int));
 printf("Size of a float :%d byte(S)\n", sizeof(float));
+return (0);
+}
