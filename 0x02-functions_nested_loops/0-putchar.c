ae9fdebe06d7c9924b6446e7f11eb290802644fc
Ange-A <angeaduhire@gmail.com>
2022-04-12 17:37:31 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,14 @@
  * Return: 0
  **/
 int main(void)
-{
-	char ermias[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c<8; c++)
-	{
-		_putchar(ermias[c]);
-        }
-_putchar('\n')
-return (0);
+{	
+         char ermias[] = "_putchar";
+         int c;
+         for (c = 0; c<8; c++)
+         {
+                   _putchar(ermias[c]);
+         }
+         _putchar('\n');
+         return (0);
 }
 
