3d6d13400d7b703f84a784a42762c768566c61a8
Ange-A <angeaduhire@gmail.com>
2022-04-13 06:43:11 -0700

    pills

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
  * _isalpha - checks for alphabetic character
  * @c: takes in a character
  * Return: 1 if letter, lowercase, uppercase; 0 otherwise
- */
+i */
 int _isalpha(int c)
 {
-if ((c >= 'A' && c<= 'z') || (c >= 'a' && c <= 'z'))
+if ((c >= 'A' && c <= 'z') || (c >= 'a' && c <= 'z'))
 {
-return(1);
+return (1);
 }
 else
 {
