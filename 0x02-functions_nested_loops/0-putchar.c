b6fdd557146953d2b795809fcb9d236306564cfa
Ange-A <angeaduhire@gmail.com>
2022-04-12 18:43:56 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
+
 /**
- * main -  entry block
- * @void: no argument
- * Return: 0
- **/
-int main(void)
-{	
-        char ermias[] = "_putchar";
-        
-        int c;
-        
-	for (c = 0; c < 8; c++)
-         {
-                   _putchar(ermias[c]);
-         }
-         _putchar('\n');
-         return (0);
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+char ermias [] = "_putchar";
+int c;
+for (c= o; c < 8; c++)
+{
+_putchar(ermias[c]);
+return (0);
+}
+_putchar('\n');
 }
 
