e656c5b3de3392d9a0b6dc2dbe1b843e0fc24550
Ange-A <angeaduhire@gmail.com>
2022-04-14 11:52:38 -0700

    super

---
+++ 0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+ * -issuper - checks upper case
+ *  @c: prints int
+ *  Return: give 1 or 0
+ */
+
+int -issupper(int c)
+{
+if (c >= 'A' && ,= 'z')
+{
+return (1);
+}
+else
+{
+return(0);
+}
+
+}
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks upper case
+ * @c: print int
+ * return: give 1 or 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && C <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
