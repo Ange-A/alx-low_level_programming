95e12ce10a7e904790b07e772fa357763c64127a
Ange-A <angeaduhire@gmail.com>
2022-04-20 10:52:58 -0700

    online

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.c"
+/**
+ * puts_half - prints half of a string
+ * @str: A pointer that will be changed
+ * Return: void when answer is correct
+ */
+void puts_half(char *str)
+{
+int i, last;
+
+i = 0;
+while(str[i] != '\0')
+{
+i++
+}
+
+last = (i + 1) /2;
+for (i = last; str(i); i++)
+{
+_putchar (str[i]);
+}
+_putchar ('\n');
+}
