5e2f32b8abcc092b6cd1d258974f30488ace2b25
Ange-A <angeaduhire@gmail.com>
2022-04-20 09:35:15 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * rev_string -  reverses a string
+ *
+ */
+
+void rev_string(char *s)
+{
+char *star_C, *end_C, C;
+int i. count;
+int length = 0;
+
+for (i = 0; s[i]; i++)
+{
+length ++;
+}
+
+count = length;
+
+start_c = s;
+end_c = s;
+
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
+
+for (i = 0; i < count / 2; i++)
+{
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+start_c ++;
+end_c --;
+}
+}
+
