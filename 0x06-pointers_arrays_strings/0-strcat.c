456f958e9028b604c54c9f44b8d5099cceda24ff
Ange-A <angeaduhire@gmail.com>
2022-04-23 06:48:58 -0700

    exit strategy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,23 +4,26 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- * @n: the number of bytes that will be used from src
+ * 
  * Return: a pointer to the resulting string
  */
-char *_strcat(char* dest, char *src)
+char *_strcat(char *dest, char *src);
 {
-	int i, j;
-	
-	i = 0;
-        j = 0;
+int i, j;
 
-        while (x = 0; dest[i] != '\0'; i++)
+i = 0;
+j= 0;
 
-        while (x= 0; src[j] != '\0' && n > 0; j++, n--, i++)
-        {
-               dest[i] = src[j];
-               
-        }
+while (dest[i] != '\0')
+	i++;
+while (src[j] != '\0')
+{
+	dest[i] = src[j];
+	j++;
+	i++;
+}
 
-        return (dest);
+dest[i] = '\0';
+
+return (dest);
 }
