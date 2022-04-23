738b14cf1cdfc3409e6017bce3eb462cc777143c
Ange-A <angeaduhire@gmail.com>
2022-04-23 05:05:18 -0700

    exit strategy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,12 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest:string to append to
+ * @dest: string to append to
  * @src: string to add
  *
- *Return: a pointer to the resulting string
+ * Return: a pointer to the resulting string
  */
+
 char *_strcat(char* dest, char *src)
 
 {
