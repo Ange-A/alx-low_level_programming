e1c5a3a9d5185a4d38fd4f1a2e52e8fe30a24acc
Ange-A <angeaduhire@gmail.com>
2022-04-23 06:41:35 -0700

    exit strategy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,9 +14,9 @@ char *_strcat(char* dest, char *src)
 	i = 0;
         j = 0;
 
-        while (x = 0; dest[i] != '\0'; i++);
+        while (x = 0; dest[i] != '\0'; i++)
 
-        while (x= 0; src[j] != '\0' && n > 0; j++, n--, i++);
+        while (x= 0; src[j] != '\0' && n > 0; j++, n--, i++)
         {
                dest[i] = src[j];
                
