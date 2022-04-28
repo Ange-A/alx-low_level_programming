638e772bab8a8fae6ac6ccef13cdb826efa9a8f9
Ange-A <angeaduhire@gmail.com>
2022-04-28 12:15:33 -0700

    five

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,14 +4,14 @@ int natural_sqrt_recursion(int n, int i);
 
 /**
  * *_sqrt_recursion - returns the natural square root of a number
- * n: the numbe to be checked
+ * @n: number to be checked
  *
  * Return: returns square root of n
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return(-1);
+		return (-1);
 	return (natural_sqrt_recursion(n, 0));
 }
 
