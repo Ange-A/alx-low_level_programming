da01716407eb30ddf5590e17d199425e7c487ba3
Ange-A <angeaduhire@gmail.com>
2022-04-28 08:20:17 -0700

    zero

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to print
+ * Return: Always 0
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*);
+	_puts_recursion(s + 1);
+
+}
