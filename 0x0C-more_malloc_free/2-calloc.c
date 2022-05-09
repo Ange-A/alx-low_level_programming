4749e1d459b750990c467431356b4a95a8db88e6
Ange-A <33396595+Ange-A@users.noreply.github.com>
2022-05-09 19:35:48 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,35 +1,31 @@
+include <stdlib.h>
 #include "main.h"
-
 /**
-<<<<<<< HEAD
- * _calloc _ allocate memory using malloc and initialize it to zero
- * @nmemb: number of elements
- * @size: size of the memory block to be allocated
- *
- * Return: pointer to the address of the memory block
- */
-=======
-* _calloc - allocate memory using malloc and initialize it to zero
-*@nmemb: number of elements
-*@size: size of the memory block to be allocated
-*
-*Return: poiner to the address of the memory block
-*/
->>>>>>> 790da39fe14da4aa6ed68526078bd3bc76741e7d
+  * _calloc - allocates memory of an array using malloc.
+  * @nmemb: number of elements in array.
+  * @size: size of elements of array.
+  *
+  * Return: NULL is size or nmemb == 0.
+  * NULL if malloc fails.
+  * Pointer to memory allocated if successful.
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *block;
+	void *p;
 	unsigned int i;
 
-	ig (nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	block = malloc(nmemb * size);
-	if (block != NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 	{
-		for (i = 0; i < (nmemb * size); i++)
-			block[i] = 0;
-		return (block);
-	}
-	else
 		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+
+	return (p);
 }
